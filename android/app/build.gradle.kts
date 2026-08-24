681dc9f7d9aa161cce2c843826ba9767acb9bb2f
NaikNithin2005 <nithinnaikr2005@gmail.com>
2026-08-24 22:54:42 +0530

    Bug fix

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 android {
     namespace = "com.example.android"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
